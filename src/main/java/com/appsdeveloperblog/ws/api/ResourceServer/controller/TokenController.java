8c6ddbba04b1068af060f49d60c7ef13786f50b5
Christopher-OOC <olojedechristopher24@gmail.com>
2024-09-02 06:36:57 +0100

    commit

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controller/TokenController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controller/TokenController.java
@@ -1,2 +1,23 @@
-package com.appsdeveloperblog.ws.api.ResourceServer.controller;public class TokenController {
+package com.appsdeveloperblog.ws.api.ResourceServer.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+    @GetMapping
+    public Map<String, Object> getToken(@AuthenticationPrincipal Jwt jwt) {
+
+        return Collections.singletonMap("principal", jwt);
+    }
+
 }
