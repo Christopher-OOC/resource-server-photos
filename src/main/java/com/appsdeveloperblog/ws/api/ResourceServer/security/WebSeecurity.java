25695c69dab6a73add53c3f9fe4fd4edf4612337
Christopher-OOC <olojedechristopher24@gmail.com>
2024-09-03 11:59:41 +0100

    commit

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSeecurity.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSeecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,12 +15,18 @@ public class WebSeecurity {
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/users").hasAuthority("SCOPE_profile")
+                        .requestMatchers(HttpMethod.GET, "/users/status/check")
+                        //.hasAuthority("SCOPE_profile")
+                        .hasRole("developer")
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
 
         return http.build();
     }
